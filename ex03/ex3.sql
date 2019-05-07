4c57dacde2e37231698b277e18f8288e26a4d686
Brent Irwin <brent@brentirwin.tv>
2019-05-07 16:20:55 -0500

    Exercise 03

---
+++ ex03/ex3.sql
@@ -0,0 +1,7 @@
+INSERT INTO person (id, first_name, last_name, age)
+  VALUES (0, 'Brent', 'Irwin', 25);
+ 
+INSERT INTO pet (id, name, breed, age, dead)
+  VALUES (0, 'Titan', 'mutt', 4, 0);
+
+INSERT INTO pet VALUES (1, 'Millie', 'American Bulldog', 9, 0);
